a36be35ee582fdce8317f68ad4462de1f7ced5c5
rufhuf <r.navratil@outlook.com>
2018-02-16 13:52:55 +0100

    Add parse first line

--- parse.php
+++ parse.php
@@ -1,5 +1,4 @@
 <?php
-
 function Params($count,$parameters){
     for($i = 1; $i < $count; $i++){
         if(($parameters[$i] == "--help") and ($count == 2)){
@@ -7,29 +6,11 @@ function Params($count,$parameters){
             exit(0);
         }else
             ErrorOutput(10);
- }
-
-  /*
-    $options = getopt('', array("help", "stats"));
-
-    foreach($options as $parameter => $value){
-
-        if($parameter == "help") {
-            if (count($options) != 1)
-                ErrorOutput(10);
-            // print napoveda
-            echo "napoveda";
-        }
-        if($parameter == "stats"){
-
-        }
     }
-  */
-
 }
 
-function ErrorOutput($index){
-    switch ($index){
+function ErrorOutput($number){
+    switch ($number){
         case 21: // Lexikální nebo syntaktická chyba.
             fwrite(STDERR,"21"); // TODO
             exit(21);
@@ -43,6 +24,15 @@ function ErrorOutput($index){
 // Osetreni vstupnich parametru.
 Params($argc, $argv);
 
-// Standartní vstup.
-$inputFile = file_get_contents("php://stdin");
-echo $inputFile;
+// Nacteni vstupu do pole.
+$inputFile = explode(PHP_EOL,file_get_contents("php://stdin"));
+
+// Osetreni a zpracovani prvniho radku.
+if (array_shift($inputFile) != ".IPPcode18")
+    ErrorOutput(21);
+
+// Zpracování instrukcí.
+foreach ($inputFile as $row){
+    echo $row;
+    echo "b";
+}
