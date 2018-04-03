3424005e0747b3f6bf26cd3a4332d78b6ec4ccc3
rufhuf <r.navratil@outlook.com>
2018-04-03 22:57:13 +0200

    ADD functions and MODIFY structure

--- interpret.py
+++ interpret.py
@@ -25,9 +25,36 @@ flags = FlagClass()  # Objekt obsahuje pouzite parametry skriptu.
 
 
 class VariableClass:
-    frame = ""
+    name = ""
     type = ""
     value = ""
+
+
+class FrameClass:
+    name = ""
+
+    def __init__(self):
+        self.variables = []  # Pole promennych ramce
+
+
+class StackClass:
+    def __init__(self):
+        self.items = []
+
+    def size(self):
+        return len(self.items)
+
+    def pop(self):
+        return self.items.pop()
+
+    def push(self, item):
+        return self.items.append(item)
+
+    def is_empty(self):
+        return self.items == []
+
+    def peek(self):
+        return self.items[len(self.items)-1]
 
 
 def params(flag):
@@ -89,6 +116,18 @@ def error_output(number):
         exit(31)
     if number == 32:  # Lexikalni chyba.
         exit(32)
+    if number == 53:  # Spatny typ promenne.
+        exit(53)
+    if number == 54:  # Pristup k neexistujici promenne.
+        exit(54)
+    if number == 55:  # Pristup k nedefinovanemu ramci.
+        exit(55)
+    if number == 56:  # Chybejici hodnota (v promenne, datovy zaboniku, zasobnik volani)
+        exit(56)
+    if number == 57:  # Deleni nulou.
+        exit(57)
+    if number == 59:  # Redefinice jiz existujici promenne.
+        exit(59)
 
 
 def xml_process(flag):
@@ -271,8 +310,7 @@ def is_symbol(argument):
         if not is_int(argument):
             error_output(32)
     elif argument.type == "var":
-        if not is_variable(argument):
-            error_output(32)
+        is_variable(argument)
     else:
         error_output(32)
 
@@ -327,8 +365,272 @@ def is_string(argument):
     return 1
 
 
+tmp_stack = StackClass()
+frames_stack = StackClass()
+global_frame = []
+data_stack = StackClass()
+
+
+def get_variable(variable):
+    global global_frame
+    global tmp_stack
+    global frames_stack
+
+    if variable.text[:2] == "LF":  # Hledana promenna je v Lokalnim ramci.
+        if frames_stack.is_empty():  # Kontrola zda lokalni ramec neni prazdny.
+            error_output(55)
+        if not frames_stack.items[frames_stack.size() - 1].variables:  # Kontrola zda ramec neni prazdny.
+            error_output(54)
+        for variable_in_frame in frames_stack.items[frames_stack.size() - 1].variables:  # Hledani promenne.
+            if variable_in_frame.name == variable.text[3:]:
+                return variable_in_frame  # Hledana promenna je nalezena.
+        error_output(54)  # Promenna se nenasla.
+
+    elif variable.text[:2] == "TF":  # Cilova promenna je v temporary ramci.
+        if tmp_stack.is_empty():  # Kontrola zda ramec neni prazdny.
+            error_output(55)
+        if not tmp_stack.items[tmp_stack.size() - 1].variables:  # Kontrola zda ramec neni prazdny.
+            error_output(54)
+        for variable_in_frame in tmp_stack.items[tmp_stack.size() - 1].variables:  # Hledani promenne.
+            if variable_in_frame.name == variable.text[3:]:
+                return variable_in_frame  # Hledana promenna je nalezena.
+        error_output(54)  # Promenna se nenasla.
+
+    elif variable.text[:2] == "GF":  # Cilova promenna je v globalnim ramci.
+        if not global_frame:  # Kontrola prazdnosti ramce
+            error_output(54)
+        for variable_in_frame in global_frame:  # Hledani promenne.
+            if variable_in_frame.name == variable.text[3:]:
+                return variable_in_frame  # Hledana promenna je nalezena.
+        error_output(54)  # Promenna se nenasla.
+
+
+def var_put_in(target, symbol_par):
+    global global_frame
+    global tmp_stack
+    global frames_stack
+    global data_stack
+
+    symbol = VariableClass()
+    if symbol_par.type == "var":
+        symbol = get_variable(symbol_par)
+    else:
+        symbol.value = symbol_par.value
+        symbol.type = symbol_par.type
+
+    if target.text[:2] == "LF":  # Cilova promenna je v Lokalnim ramci.
+        if frames_stack.is_empty():  # Kontrola zda lokalni ramec neni prazdny.
+            error_output(55)
+        if not frames_stack.items[frames_stack.size() - 1].variables:  # Kontrola zda ramec neni prazdny.
+            error_output(54)
+        for variable_in_frame in frames_stack.items[frames_stack.size() - 1].variables:  # Hledani promenne.
+            if variable_in_frame.name == target.text[3:]:
+                variable_in_frame.value = symbol.value
+                variable_in_frame.type = symbol.type
+                return
+        error_output(54)  # Promenna se nenasla.
+
+    elif target.text[:2] == "TF":  # Cilova promenna je v temporary ramci.
+        if tmp_stack.is_empty():  # Kontrola zda ramec neni prazdny.
+            error_output(55)
+        if not tmp_stack.items[tmp_stack.size() - 1].variables:  # Kontrola zda ramec neni prazdny.
+            error_output(54)
+        for variable_in_frame in tmp_stack.items[tmp_stack.size() - 1].variables:  # Hledani promenne.
+            if variable_in_frame.name == target.text[3:]:
+                variable_in_frame.value = symbol.value
+                variable_in_frame.type = symbol.type
+                return
+        error_output(54)  # Promenna se nenasla.
+
+    elif target.text[:2] == "GF":  # Cilova promenna je v globalnim ramci.
+        if not global_frame:  # Kontrola prazdnosti ramce
+            error_output(54)
+        for variable_in_frame in global_frame:  # Hledani promenne.
+            if variable_in_frame.name == target.text[3:]:
+                variable_in_frame.value = symbol.value
+                variable_in_frame.type = symbol.type
+                return
+        error_output(54)  # Promenna se nenasla.
+
+
 def interpret(instructions_list):
-    print("TODO")
+    global global_frame
+    global tmp_stack
+    global frames_stack
+    global data_stack
+
+    x = 0
+    while x < len(instructions_list):
+
+        if instructions_list[x].opcode == "CREATEFRAME":
+            frame = FrameClass()  # Vytvoreni prazdneho ramce.
+            frame.name = "TF"  # Pojmenovani ramce.
+            while not tmp_stack.is_empty():  # Ulozeni ramce na TMP zasobnik.
+                tmp_stack.pop()
+            tmp_stack.push(frame)
+
+        elif instructions_list[x].opcode == "PUSHFRAME":
+            if tmp_stack.is_empty():
+                error_output(55)
+            tmp_stack.items[0].name = "LF"
+            frames_stack.push(tmp_stack.pop())
+
+        elif instructions_list[x].opcode == "POPFRAME":
+            if frames_stack.is_empty():
+                error_output(55)
+            frames_stack.items[0].name = "TF"
+            tmp_stack.push(frames_stack.pop())
+
+        elif instructions_list[x].opcode == "DEFVAR":
+            variable = VariableClass()  # Vytvoreni objektu pro promennouo.
+            variable.name = instructions_list[x].arguments[0].text[3:]  # Ulozeni jejiho jmena.
+            tmp_frame_type = instructions_list[x].arguments[0].text[:2]  # Ulozeni jejiho typu.
+            if tmp_frame_type == "TF":
+                if tmp_stack.is_empty():
+                    error_output(55)
+                # if is_variable_in_frame(variable.name, tmp_stack, frames_stack, global_frame):
+                    # error_output(59)
+                tmp_stack.items[0].variables.append(variable)  # TODO co kdyz je to prazdny nazev
+            elif tmp_frame_type == "LF":
+                if frames_stack.is_empty():
+                    error_output(55)
+                # if is_variable_in_frame(variable.name, tmp_stack, frames_stack, global_frame):
+                    # error_output(59)
+                frames_stack.items[frames_stack.size() - 1].variables.append(variable)
+            elif tmp_frame_type == "GF":
+                # if is_variable_in_frame(variable.name, tmp_stack, frames_stack, global_frame):
+                    # error_output(59)
+                global_frame.append(variable)
+
+        elif instructions_list[x].opcode == "PUSHS":
+            if instructions_list[x].arguments[0].type == "var":
+                variable_to_symb = get_variable(instructions_list[x].arguments[0])
+                variable_to_symb.name = ""
+                if not variable_to_symb.value:  # Promenna neobsahuje hodnotu.
+                    error_output(56)
+                data_stack.push(variable_to_symb)  # Vlozeni promenne na zasobnik.
+            else:
+                symbol = VariableClass()  # Vytvoreni objektu pro vlozeni na datovy zasobnik.
+                symbol.value = instructions_list[x].arguments[0].text
+                symbol.type = instructions_list[x].arguments[0].type
+                data_stack.push(symbol)  # Vlozeni symbolu na zasobnik.
+
+        elif instructions_list[x].opcode == "POPS":
+            if data_stack.is_empty():
+                error_output(56)
+            var_put_in(instructions_list[x].arguments[0], data_stack.pop())
+
+        elif instructions_list[x].opcode == "MOVE":
+            variable = instructions_list[x].arguments[0]
+            symbol = VariableClass()
+            symbol.type = instructions_list[x].arguments[1].type
+            symbol.value = instructions_list[x].arguments[1].text
+            var_put_in(variable, symbol)  # Priradi symbol promenne.
+
+        elif instructions_list[x].opcode == "ADD":
+            result = 0
+            for y in range(1, 3):
+                symbol = instructions_list[x].arguments[y]
+                if symbol.type == "var":
+                    variable_tmp = get_variable(symbol)
+                    if not variable_tmp.value:
+                        error_output(56)
+                    if not variable_tmp.type == "int":
+                        error_output(53)
+                    result += int(variable_tmp.value)
+                else:
+                    if not symbol.type == "int":
+                        error_output(53)
+                    result += int(symbol.text)
+            result_var = VariableClass()
+            result_var.type = "int"
+            result_var.value = str(result)
+            var_put_in(instructions_list[x].arguments[0], result_var)
+
+        elif instructions_list[x].opcode == "SUB":
+            result = 0
+            for y in range(1, 3):
+                symbol = instructions_list[x].arguments[y]
+                if symbol.type == "var":
+                    variable_tmp = get_variable(symbol)
+                    if not variable_tmp.value:
+                        error_output(56)
+                    if not variable_tmp.type == "int":
+                        error_output(53)
+                    if y == 1:
+                        result += int(variable_tmp.value)
+                    else:
+                        result -= int(variable_tmp.value)
+                else:
+                    if not symbol.type == "int":
+                        error_output(53)
+                    if y == 1:
+                        result += int(symbol.text)
+                    else:
+                        result -= int(symbol.text)
+            result_var = VariableClass()
+            result_var.type = "int"
+            result_var.value = str(result)
+            var_put_in(instructions_list[x].arguments[0], result_var)
+
+        elif instructions_list[x].opcode == "MUL":
+            result = 0
+            for y in range(1, 3):
+                symbol = instructions_list[x].arguments[y]
+                if symbol.type == "var":
+                    variable_tmp = get_variable(symbol)
+                    if not variable_tmp.value:
+                        error_output(56)
+                    if not variable_tmp.type == "int":
+                        error_output(53)
+                    if y == 1:
+                        result += int(variable_tmp.value)
+                    else:
+                        result *= int(variable_tmp.value)
+                else:
+                    if not symbol.type == "int":
+                        error_output(53)
+                    if y == 1:
+                        result += int(symbol.text)
+                    else:
+                        result *= int(symbol.text)
+            result_var = VariableClass()
+            result_var.type = "int"
+            result_var.value = str(result)
+            var_put_in(instructions_list[x].arguments[0], result_var)
+
+        elif instructions_list[x].opcode == "IDIV":
+            result = 0
+            for y in range(1, 3):
+                symbol = instructions_list[x].arguments[y]
+                if symbol.type == "var":
+                    variable_tmp = get_variable(symbol)
+                    if not variable_tmp.value:
+                        error_output(56)
+                    if not variable_tmp.type == "int":
+                        error_output(53)
+                    if y == 1:
+                        result += int(variable_tmp.value)
+                    else:
+                        if str(variable_tmp.value) == "0":
+                            error_output(57)
+                        result = int(result / int(variable_tmp.value))
+                else:
+                    if not symbol.type == "int":
+                        error_output(53)
+                    if y == 1:
+                        result += int(symbol.text)
+                    else:
+                        if symbol.text == "0":
+                            error_output(57)
+                        result = int(result / int(symbol.text))
+            result_var = VariableClass()
+            result_var.type = "int"
+            result_var.value = str(result)
+            var_put_in(instructions_list[x].arguments[0], result_var)
+
+        x += 1
+    print("Petrohrad")
 
 
 # Zpracovani parametru.
